121a13e4b0e1e9280dd0c4aa8b8517349e297ca0
Veronika-Bortkova <shishiga79@gmail.com>
2025-11-25 22:24:53 +0200

    hw-1 finished-1

--- HW-1/HW-1-4/main.js
+++ HW-1/HW-1-4/main.js
@@ -1,7 +1,7 @@
 // – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
-let name = prompt("Введить ваше Ім'я", "Veronika");
-let middle_Name = prompt("Введить ваше Ім'я по-батькові", "Olexandrivna");
-let age = prompt("Введить ваш вік", "45");
+let name = prompt("Введить Ваше Ім'я", "Veronika");
+let middle_Name = prompt("Введить Ваше Ім'я по-батькові", "Olexandrivna");
+let age = prompt("Введить Ваш вік", "45");
 // console.log(name + " " + middle_Name + " " + age);
 // или
 console.log(`${name} ${middle_Name} ${age}`)
