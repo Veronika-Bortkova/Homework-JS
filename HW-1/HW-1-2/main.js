5b3fc59ce68751c82a581226254a725a0e8cf9ca
Veronika-Bortkova <shishiga79@gmail.com>
2025-11-25 22:13:05 +0200

    hw-1 finished

---
+++ HW-1/HW-1-2/main.js
@@ -0,0 +1,10 @@
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+let firstName = "Veronika";
+let middleName = "Olexandrivna";
+let lastName = "Bortkova";
+let person = (lastName +" "+firstName+" "+middleName);
+// или
+// let person = (`${lastName} ${firstName} ${middleName}`);
+console.log(person);
+
+
