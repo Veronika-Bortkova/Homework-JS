843865ea7d641c2c63a5d55971324954dd78664e
Veronika-Bortkova <shishiga79@gmail.com>
2025-11-27 16:15:40 +0200

    hw-2 finished

--- HW-2/HW-2-12/main.js
+++ HW-2/HW-2-12/main.js
@@ -1,3 +1,5 @@
+// з файлу arrays.js ( лежить у вкладеннях)  взяти масив coursesAndDurationArray. За допомогою іф перевірити кожен його елемент на тривалість навчання. У випадку, якщо тривалість довша за 5 місяців, вивести в консоль “Супер”.
+
 let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
     {title: 'Java Complex', monthDuration: 6},
